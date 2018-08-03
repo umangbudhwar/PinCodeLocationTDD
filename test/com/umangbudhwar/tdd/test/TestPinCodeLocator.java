ef6dc966f05cff9ce94d77cd064e7388e4d9ae8e
Umang Budhwar <umangbudhwar@gmail.com>
2018-08-03 12:19:15 +0530

    Update TestPinCodeLocator.java

--- test/com/umangbudhwar/tdd/test/TestPinCodeLocator.java
+++ test/com/umangbudhwar/tdd/test/TestPinCodeLocator.java
@@ -11,36 +11,25 @@ import com.umangbudhwar.tdd.PinCodeLocator;
 
 public class TestPinCodeLocator {
 
-	static PinCodeLocator locator;
-	@BeforeClass
-	public static void beforeClass()
-	{
-		locator = new PinCodeLocator();
-	}
-	
 	@Test
 	public void testIfPinCodeLocatorClassExists() {
-		assertNotNull(locator);		
+		PinCodeLocator locator = new PinCodeLocator(null);
+		assertNotNull(locator);
 	}
-	
-	@Test
-	public void testIfGetLocationMethodExists() {
-		LocatorService service = Mockito.mock(LocatorService.class);
-		PinCodeLocator locator2 = new PinCodeLocator(service);
-		locator.getLocation(211006);		
-	}
-	
+
 	@Test
 	public void testIfGetLocationMethodReturnsString() {
-		String location = locator.getLocation(211006);		
+		LocatorService service = Mockito.mock(LocatorService.class);
+		PinCodeLocator locator = new PinCodeLocator(service);
+		String location = locator.getLocation(211006);
 	}
-	
+
 	@Test
 	public void testIf211006ReturnsAllahabad() {
 		LocatorService service = Mockito.mock(LocatorService.class);
-		
+
 		Mockito.when(service.getLocation(211006)).thenReturn("Allahabad");
-		
+
 		PinCodeLocator locator2 = new PinCodeLocator(service);
 		String area = locator2.getLocation(211006);
 		assertEquals("Allahabad", area);
